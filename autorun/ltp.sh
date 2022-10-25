pevik/rapido

commit 1783f67d7c85ba89005059fb942493e178321889

Add support for LTP
The Linux Test Project [1] is a collection of tools for testing the
Linux kernel and related features.
These tests can/will harm the system you run them on, so running them
inside virtual machines is neccessary for developing new testcases.

Rapido can build a ramdisk image with the complete LTP binaries and
execute a single test or the included testrunner.

[1] https://github.com/linux-test-project/ltp/

Signed-off-by: Michael Moese <mmoese@suse.de>
Signed-off-by: David Disseldorp <ddiss@suse.de>

autorun/ltp.sh
@@ -1,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2019, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+
+if [ ! -f /vm_autorun.env ]; then
+	echo "Error: autorun scripts must be run from within an initramfs VM"
+	exit 1
+fi
+
+. /vm_autorun.env
+
+_vm_ar_dyn_debug_enable
+
+set -x
+
+# ltp requires a few preexisting users/groups
+xid="2000"
+for ug in nobody bin daemon; do
+	echo "${ug}:x:${xid}:${xid}:${ug} user:/:/sbin/nologin" >> /etc/passwd
+	echo "${ug}:x:${xid}:" >> /etc/group
+	((xid++))
+done
+
+cd ${LTP_DIR} || _fatal
+if [ -n "$LTP_AUTORUN_CMD" ]; then
+	echo "Running LTP Command: $LTP_AUTORUN_CMD"
+	eval "$LTP_AUTORUN_CMD"
+fi

cut/ltp.sh
@@ -1,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (C) SUSE LINUX GmbH 2019, all rights reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) version 3.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args
+_rt_require_conf_dir LTP_DIR
+
+"$DRACUT" \
+	--install "tail blockdev ps rmdir resize dd grep find df mkfs which \
+		perl awk bc touch cut chmod true false unlink mktemp getfattr \
+		setfattr attr killall hexdump sync id sort uniq date expr tac \
+		diff head dirname seq basename tee egrep yes dmsetup chattr \
+		lsattr cmp stat hostname getconf md5sum od wc tr xargs sysctl \
+		link truncate quota quotacheck quotaon vgremove chgrp du fgrep \
+		pgrep tar rev kill fdformat ldd free losetup \
+		${LTP_DIR}/bin/* ${LTP_DIR}/testcases/bin/*" \
+	--include "$RAPIDO_DIR/autorun/ltp.sh" "/.profile" \
+	--include "$RAPIDO_DIR/rapido.conf" "/rapido.conf" \
+	--include "$RAPIDO_DIR/vm_autorun.env" "/vm_autorun.env" \
+	--include "$LTP_DIR" "$LTP_DIR"  \
+	--add-drivers "loop" \
+	--modules "bash base" \
+	$DRACUT_EXTRA_ARGS \
+	$DRACUT_OUT || _fail "dracut failed"
+
+_rt_xattr_vm_networkless_set "$DRACUT_OUT"		# *disable* network
+_rt_xattr_vm_resources_set "$DRACUT_OUT" "2" "2048M"	# 2 vCPUs, 2G RAM

rapido.conf.example
@@ -227,3 +227,13 @@ INITIATOR_IQNS="iqn.2007-10.com.github:sahlberg:libiscsi:iscsi-test \
 # when issuing a SendTargets discovery request.
 #INITIATOR_DISCOVERY_ADDR=""
 ################################
+
+########## cut/ltp.sh ##########
+# LTP_DIR should correspond to the ltp directory tree after make install.
+# e.g. LTP_DIR="/opt/ltp"
+#LTP_DIR=""
+# LTP_AUTORUN_CMD can be set to automatically run one or more testcases from
+# within the ltp directory.
+# e.g. LTP_AUTORUN_CMD="./runltp"
+#LTP_AUTORUN_CMD=""
+################################

commit 77f3f03c991ff1532910c3d948cac506b8d9bb5b

ltp: Remove extra install of LTP dirs, update PATH
Whole LTP is added via --include "$LTP_DIR" "$LTP_DIR",
therefore remove adding separately bin/* and testcases/bin/*.
But that requires to properly setup PATH to these 2 directories.

While at it, add also LTP root directory, in order to be able to call
runltp without specifying its path.

Fixes: 1783f67 ("Add support for LTP")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

autorun/ltp.sh
@@ -27,6 +27,8 @@ for ug in nobody bin daemon; do
 	((xid++))
 done
 
+export PATH="$LTP_DIR:$LTP_DIR/bin:$LTP_DIR/testcases/bin:$PATH"
+
 cd ${LTP_DIR} || _fatal
 if [ -n "$LTP_AUTORUN_CMD" ]; then
 	echo "Running LTP Command: $LTP_AUTORUN_CMD"

cut/ltp.sh
@@ -36,8 +36,7 @@ fses=(btrfs exfat ext2 ext3 ext4 fuse ntfs vfat xfs)
 		${fses[*]/#/mkfs.} mktemp ${fses[*]/#/mount.} od parted perl pgrep ping
 		ping6 pkill ps quota quotacheck quotaon resize rev rmdir sed seq
 		setfacl setfattr sort stat strace sync sysctl tac tail tar tc tee touch
-		tr true truncate uniq unlink vgremove wc which xargs xxd yes
-		${LTP_DIR}/bin/* ${LTP_DIR}/testcases/bin/*" \
+		tr true truncate uniq unlink vgremove wc which xargs xxd yes" \
 	--include "$LTP_DIR" "$LTP_DIR"  \
 	--include "$config" /.config \
 	--add-drivers "loop" \